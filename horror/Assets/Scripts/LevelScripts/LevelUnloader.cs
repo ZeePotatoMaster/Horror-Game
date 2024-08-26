862de02b912160a889812c6f75f508ac8f9255a9
wiggle <wigglewwoggle@gmail.com>
2024-08-25 20:14:24 -0500

    nate higgers is calling

--- horror/Assets/Scripts/LevelScripts/LevelUnloader.cs
+++ horror/Assets/Scripts/LevelScripts/LevelUnloader.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class LevelUnloader : MonoBehaviour
 {
+
+    public GameObject singleplayerPlayer;
+    public int maxDistanceFromPlayer;
+
+    public List<GameObject> levelRooms;
+
+    public GameObject parentLevel;
+
+    //UNUSED FOR NOW
+    public List<GameObject> multiplayerPlayers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //note: revisit once random level loading is enabled and refactor as the third process in loading steps?
+
+        populateFromScene();
+
+        //default of 100, remove later
+        maxDistanceFromPlayer = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        toggleRoomsLoaded();
+    }
+
+    void populateFromScene() {
+
+        levelRooms.Clear();
+
+        foreach (Transform child in parentLevel.transform) {
+
+            levelRooms.Add(child.gameObject);
+        }
+    }
+
+    void toggleRoomsLoaded() {
+
+        foreach (GameObject room in levelRooms) {
+
+            if (Vector3.Distance(singleplayerPlayer.transform.position, room.transform.position) > maxDistanceFromPlayer) {
+
+                room.SetActive(false);
+            } else {
+
+                room.SetActive(true);
+            }
+        }
     }
 }
